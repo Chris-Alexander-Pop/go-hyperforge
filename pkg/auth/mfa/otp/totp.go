63aa1be9adc1db6ffb9654e410fc54a0bfaf3a39
Chris-Alexander-Pop <144747578+Chris-Alexander-Pop@users.noreply.github.com>
2026-03-20 14:14:29 +0000

    ⚡ Bolt: Optimize TOTP code formatting by avoiding fmt.Sprintf

--- pkg/auth/mfa/otp/totp.go
+++ pkg/auth/mfa/otp/totp.go
@@ -14,6 +14,7 @@ import (
 	"encoding/base32"
 	"encoding/binary"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -120,8 +121,12 @@ func (t *TOTP) generateCodeForCounter(secret string, counter uint64) (string, er
 	code = code % mod
 
 	// Pad with zeros
-	format := fmt.Sprintf("%%0%dd", t.config.Digits)
-	return fmt.Sprintf(format, code), nil
+	str := strconv.FormatInt(code, 10)
+	pad := t.config.Digits - len(str)
+	if pad > 0 {
+		return "00000000000000000000"[:pad] + str, nil
+	}
+	return str, nil
 }
 
 // Validate checks if a code is valid for the given secret.
