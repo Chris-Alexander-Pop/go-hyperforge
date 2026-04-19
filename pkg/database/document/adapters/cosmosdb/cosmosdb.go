52ea5cdac01b7c8d1baba58daeeaa5e96563dfa0
Chris-Alexander-Pop <144747578+Chris-Alexander-Pop@users.noreply.github.com>
2026-04-19 14:31:52 +0000

    Fix NoSQL Injection in CosmosDB adapter
    Mitigates a NoSQL injection vulnerability in `pkg/database/document/adapters/cosmosdb/cosmosdb.go` where map keys were concatenated unrestrictedly into the dynamic query builder. Validates all keys against an alphanumeric and dot-underscore regex allowlist.

--- pkg/database/document/adapters/cosmosdb/cosmosdb.go
+++ pkg/database/document/adapters/cosmosdb/cosmosdb.go
@@ -8,10 +8,14 @@ import (
 	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"regexp"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
 	"github.com/chris-alexander-pop/system-design-library/pkg/database/document"
 	"github.com/chris-alexander-pop/system-design-library/pkg/errors"
 )
+
+var queryKeyRegex = regexp.MustCompile(`^[a-zA-Z0-9_.]+$`)
 
 // NOTE: This implementation assumes the Azure SDK for Go is available.
 // If not, please run: go get github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos
@@ -83,7 +87,10 @@ func (a *Adapter) Find(ctx context.Context, collection string, query map[string]
 		return nil, errors.Internal("failed to get container client", err)
 	}
 
-	queryText, queryParams := buildQuery(query)
+	queryText, queryParams, err := buildQuery(query)
+	if err != nil {
+		return nil, err
+	}
 
 	// Detect partition key from query
 	pk := azcosmos.NewPartitionKey() // Default to empty partition key (cross-partition query)
@@ -171,7 +178,7 @@ func (a *Adapter) Close() error {
 	return nil
 }
 
-func buildQuery(query map[string]interface{}) (string, []azcosmos.QueryParameter) {
+func buildQuery(query map[string]interface{}) (string, []azcosmos.QueryParameter, error) {
 	// Pre-calculate estimated size to reduce re-allocations
 	// Base: "SELECT * FROM c" (15)
 	// Per param: " WHERE " (7) or " AND " (5) + "c." (2) + key (~10) + " = " (3) + "@p" (2) + digits (1-3)
@@ -187,13 +194,17 @@ func buildQuery(query map[string]interface{}) (string, []azcosmos.QueryParameter
 	sb.WriteString("SELECT * FROM c")
 
 	if len(query) == 0 {
-		return sb.String(), nil
+		return sb.String(), nil, nil
 	}
 
 	sb.WriteString(" WHERE ")
 	params := make([]azcosmos.QueryParameter, 0, len(query))
 	i := 0
 	for k, v := range query {
+		if !queryKeyRegex.MatchString(k) {
+			return "", nil, errors.InvalidArgument(fmt.Sprintf("invalid query key format: %s", k), nil)
+		}
+
 		if i > 0 {
 			sb.WriteString(" AND ")
 		}
@@ -213,5 +224,5 @@ func buildQuery(query map[string]interface{}) (string, []azcosmos.QueryParameter
 		i++
 	}
 
-	return sb.String(), params
+	return sb.String(), params, nil
 }
